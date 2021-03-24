0d06482e757b9634752a0a0fb174370a545ab885
Kristina Al-ashqar <kristinaal-ashqar@Kristinas-MacBook-Air.local>
2021-03-24 15:06:33 -0500

    added content to servivce worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,12 @@ const CACHE_NAME = "budget-static-cash-v1"
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     '/',
-    'style.css', 
+    '/index.html',
+    '/styles.css', 
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/index.js',
+    '/manifest.webmanifest',
     
   
 ];
