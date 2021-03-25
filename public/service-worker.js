69cde90053ea050236446d20460a1b9a5503b8a2
Kristina Al-ashqar <kristinaal-ashqar@Kristinas-MacBook-Air.local>
2021-03-24 19:21:06 -0500

    fetch call working

--- public/service-worker.js
+++ public/service-worker.js
@@ -53,7 +53,23 @@ self.addEventListener('activate' ,evt =>{
 
 //this code here
 self.addEventListener('fetch', evt => {
-  console.log('hitb fetch')
+
+  if(evt.request.url.includes('/api/transaction')){
+    evt.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(evt.request).then(response=>{
+          if (response.status ===200){
+            cache.put(evt.request.url, response.clone())
+          }
+          return response
+        })
+        .catch(err =>{
+          return cache.match(evt.request)
+        })
+      })
+    )
+  }
+  
   evt.respondWith(
     caches.open(CACHE_NAME).then(cache =>{
       return cache.match(evt.request).then(response =>{
